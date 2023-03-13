9d2880423c4c1aeb494c539c58e5dc9142612b79
usif <82738210+itsusif@users.noreply.github.com>
2023-03-13 03:08:37 +0200

    Update Events.ts

--- handlers/Events.ts
+++ handlers/Events.ts
@@ -12,13 +12,4 @@ module.exports = (client) => {
             }
         }
     })
-    // const eventFiles = fs.readdirSync('./src/bot/events').filter(file => file.endsWith('.ts'));
-    // for (const file of eventFiles) {
-    //     const event = require(`../events/${file}`);
-    //     if (event.once) {
-    //         client.once(event.name, (...args) => event.execute(client, ...args));
-    //     } else {
-    //         client.on(event.name, (...args) => event.execute(client, ...args));
-    //     }
-    // }
 }
